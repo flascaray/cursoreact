c7e07647c6a6fa2e766743fa88094b8db8bbc29e
Felipe Lascaray <lascarayf@gmail.com>
2019-03-14 10:53:54 -0300

    first

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import { add } from './math.js'
+
 const PI = 3.141592
 
-const acc = 0
+let acc = 0
 acc = acc + 1
 acc = acc + 1
 
-console.log("PI", PI)
-console.log("acc", acc)
+console.log('PI', PI)
+console.log('acc', acc)
+
+console.log(add(3, 4))
